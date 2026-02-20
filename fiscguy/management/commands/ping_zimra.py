be18dba8d8a251f6e093f2ecf362476f88cb216a
casy <cassymyo@gmail.com>
2026-02-20 16:15:58 +0200

    `Refactor and add new features to the Fiscguy project`

---
+++ fiscguy/management/commands/ping_zimra.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from zimra_base import ZIMRAClient
+import time
+from fiscguy.models import Device
+from loguru import logger
+from fiscguy.api import _get_client
+
+
+class Command(BaseCommand):
+    help = "Ping ZIMRA periodically"
+
+    def handle(self, *arg, **optioons): 
+        logger.info("Ping for Initiatiated")
+        while True:
+            try:
+                res =  _get_client().ping()
+                time.sleep(res['reportingFrequency'])
+            except Exception as e:
+                logger.error(f"Failed to ping zimra: {e}")
