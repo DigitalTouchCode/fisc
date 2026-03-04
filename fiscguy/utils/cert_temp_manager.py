8cfc01989508d871bbb9e23418dae618a417996c
casper moyo <cassymyo@gmail.com>
2026-03-04 10:04:14 +0200

    style: format files with Black

--- fiscguy/utils/cert_temp_manager.py
+++ fiscguy/utils/cert_temp_manager.py
@@ -16,7 +16,7 @@ class CertTempManager:
         self._pem_path = self._temp_dir / "client.pem"
         self._key_path = self._temp_dir / "key.pem"
 
-        self._pem_path.write_text(fr"{cert_pem}\{key_pem}")
+        self._pem_path.write_text(rf"{cert_pem}\{key_pem}")
         self._key_path.write_text(f"{key_pem}")
         self._closed = False
 
