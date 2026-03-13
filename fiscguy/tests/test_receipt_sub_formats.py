e7f1e5192aa4a3310989f44d3e3b22f170d8db11
casy <cassymyo@gmail.com>
2026-03-13 16:49:44 +0200

    fix: upgrade black version

---
+++ fiscguy/tests/test_receipt_sub_formats.py
@@ -0,0 +1,9 @@
+# 1 normal receipt one tax line
+# 2 normal receipt multiple tax lines
+# 3 normal credit note one tax line
+# 4 normal credit note multiple tax lines
+
+
+class TestReceiptSubmissionFormats:
+    def test_receipt_sub_formats(self):
+        assert True
