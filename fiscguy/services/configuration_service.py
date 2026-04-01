8a67ade28b63755dc8bd72b27965768afff6a6c0
cassymyo-spec <cassymyo@gmail.com>
2026-04-01 17:28:36 +0200

    Chore: Return config object instead of raw config data

--- fiscguy/services/configuration_service.py
+++ fiscguy/services/configuration_service.py
@@ -16,7 +16,7 @@ class ConfigurationService:
     def config(self) -> Configuration:
         config_data = self.get_configuration()
         config = self.create_or_update_config(config_data)
-        return config_data
+        return config
 
     def get_configuration(self) -> Dict[str, any]:
         try:
